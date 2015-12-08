8b09e82ddcda81659da9e96f865d8d20cc71ae0d
Tero Tolonen <digikansio@gmail.com>
2015-12-08 18:54:51 +0200

    "commit message"

--- release/ASTEval-0.89.js
+++ release/ASTEval-0.89.js
@@ -343,51 +343,30 @@
         a = _toValue(a);
         b = _toValue(b);
 
-        if (true) {
+        // ?? should result be object with value ?
 
-          // ?? should result be object with value ?
+        if (node.operator == "+") return node.eval_res = a + b;
+        if (node.operator == "-") return node.eval_res = a - b;
+        if (node.operator == "*") return node.eval_res = a * b;
+        if (node.operator == "/") return node.eval_res = a / b;
+        if (node.operator == "<") return node.eval_res = a < b;
+        if (node.operator == "<=") return node.eval_res = a <= b;
+        if (node.operator == ">") return node.eval_res = a > b;
+        if (node.operator == ">=") return node.eval_res = a >= b;
+        if (node.operator == "&") return node.eval_res = a & b;
+        if (node.operator == "|") return node.eval_res = a | b;
+        if (node.operator == "<<") return node.eval_res = a << b;
+        if (node.operator == ">>") return node.eval_res = a >> b;
+        if (node.operator == ">>>") return node.eval_res = a >>> b;
 
-          if (node.operator == "+") node.eval_res = a + b;
-          if (node.operator == "-") node.eval_res = a - b;
-          if (node.operator == "*") node.eval_res = a * b;
-          if (node.operator == "/") node.eval_res = a / b;
-          if (node.operator == "<") node.eval_res = a < b;
-          if (node.operator == "<=") node.eval_res = a <= b;
-          if (node.operator == ">") node.eval_res = a > b;
-          if (node.operator == ">=") node.eval_res = a >= b;
-          if (node.operator == "&") node.eval_res = a & b;
-          if (node.operator == "|") node.eval_res = a | b;
-          if (node.operator == "<<") node.eval_res = a << b;
-          if (node.operator == ">>") node.eval_res = a >> b;
-          if (node.operator == ">>>") node.eval_res = a >>> b;
+        if (node.operator == "==") return node.eval_res = a == b;
+        if (node.operator == "!=") return node.eval_res = a != b;
+        if (node.operator == "===") return node.eval_res = a === b;
+        if (node.operator == "!==") return node.eval_res = a !== b;
+        if (node.operator == "%") return node.eval_res = a % b;
+        if (node.operator == "instanceof") return node.eval_res = a instanceof b;
 
-          if (node.operator == "==") node.eval_res = a == b;
-          if (node.operator == "!=") node.eval_res = a != b;
-          if (node.operator == "===") node.eval_res = a === b;
-          if (node.operator == "!==") node.eval_res = a !== b;
-          if (node.operator == "%") node.eval_res = a % b;
-          if (node.operator == "instanceof") node.eval_res = a instanceof b;
-        } else {
-          console.error("Undefined variable in BinaryExpression");
-        }
-        /*
-        Equal (==)	Returns true if the operands are equal.	3 == var1
-        "3" == var1
-        3 == '3'
-        Not equal (!=)	Returns true if the operands are not equal.	var1 != 4
-        var2 != "3"
-        Strict equal (===)	Returns true if the operands are equal and of the same type. See also Object.is and sameness in JS.	3 === var1
-        Strict not equal (!==)	Returns true if the operands are of the same type but not equal, or are of different type.	var1 !== "3"
-        3 !== '3'
-        Greater than (>)	Returns true if the left operand is greater than the right operand.	var2 > var1
-        "12" > 2
-        Greater than or equal (>=)	Returns true if the left operand is greater than or equal to the right operand.	var2 >= var1
-        var1 >= 3
-        Less than (<)	Returns true if the left operand is less than the right operand.	var1 < var2
-        "2" < 12
-        Less than or equal (<=)	Returns true if the left operand is less than or equal to the right operand.	var1 <= var2
-        var2 <= 5
-        */
+        console.error("Undefined variable in BinaryExpression");
       };
 
       /**
